d88ae2ca5129024d79d22b292b3d8d7e01ca53b7
nylander <Johan.Nylander@nrm.se>
2024-02-20 17:51:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Saturation plots - P-dist vs. Tree-dist
+
+- Last modified: tis feb 20, 2024  05:48
+- Sign: JN
+
+![Saturation plots](img/plots.png)
+
+## Description
+
+Code for generating saturation plots used in [Klopfstein et al.,
+2013](doi:10.1371/journal.pone.0069344) for displaying the relationship between
+the uncorrected (P-) distance and the distance on a phylogenetic tree.
+
+## Requirements
+
+[R](https://www.r-project.org/) with R-package
+[ape](https://cloud.r-project.org/web/packages/ape/index.html).
+
+Recently tested with R v4.3.2 and ape v5.7.1.
+
+## Input
+
+1. Multiple sequence alignment in fasta format ([example](data/data.fas))
+2. Tree with tip labels corresponding to sequence labels
+   ([example](data/tree.nex))
+
+## Example
+
+    > library("ape")
+    > source("src/saturation.plot.R")
+    > phy <- read.nexus("data/tree.nex")
+    > dna <- read.dna("data/data.fas", format="fasta")
+    > par(mfrow=c(2, 2))
+    > saturation.plot(phy, dna, main="default")
+    > saturation.plot(phy, dna, main="no box or regression",
+                    bg=FALSE, reg=FALSE)
+    > saturation.plot(phy, dna, main="change some settings",
+                    bg=FALSE, col="red", xlim=c(15, 75),
+                    ylim=c(0, 0.16), cex=0.5)
+    > saturation.plot(phy, dna, main="other colors and symbols",
+                    bg=TRUE, col="yellow", bg.col="darkgray",
+                    pch=24, cex=1.5)
+
---
+++ src/saturation.plot.R
@@ -0,0 +1,74 @@
+"saturation.plot" <- function(phy, dna, regression=TRUE, bg=TRUE, pch=NULL,
+                            bg.col="black", col=NULL, xlab="Distance on tree",
+                            ylab="p-distance", ...) {
+
+  ## Saturation plot: Uncorrected P-distance vs distance on tree.
+  ## By: Seraina Klopstein, 2012 (modified by Johan Nylander, 2013)
+  ## Last modified: tis feb 20, 2024  05:42
+
+  require("ape")
+
+  if (!inherits(phy, "phylo")) {
+    stop("object 'phy' is not of class 'phylo'")
+  }
+  if (!inherits(dna, "DNAbin")) {
+    stop("object 'dna' is not of class 'DNAbin'")
+  }
+  if (!is.matrix(dna)) {
+    dna <- as.matrix(dna)
+  }
+  ## Sort sequences according to taxon names in tree file
+  sortingSeq <- match(phy$tip.label, dimnames(dna)[[1]])
+  dna <- dna[sortingSeq, ]
+  pDist <- dist.dna(dna, model="raw", as.matrix=FALSE, pairwise.deletion=TRUE)
+  MBDist <- cophenetic(phy)
+  taxonList <- attr(pDist, "Labels")
+  nbPairs <- (length(taxonList)^2 - length(taxonList)) / 2
+  distTable <- matrix(ncol=2, nrow=nbPairs)
+  helpMatrix <- matrix(ncol=length(taxonList), nrow=length(taxonList))
+  counter1 <- 1
+  counter2 <- 1
+  for (j in 1:length(taxonList)) {
+    for (k in 1:length(taxonList)) {
+      if (k <= counter2) {
+        helpMatrix[j, k] <- NA
+      } else {
+        helpMatrix[j, k] <- counter1
+        counter1 <- counter1 + 1
+      }
+    }
+    counter2 <- counter2 + 1
+  }
+  ## Fill matrix with pairs, divide distance by two
+  for (i in 1:nbPairs) {
+    distTable[i, 1] <- pDist[i] / 2
+    distTable[i, 2] <- MBDist[which(helpMatrix[] == i)] / 2
+  }
+  ## Linear regression
+  fit <- lm(distTable[, 1] ~ distTable[, 2])
+  ## Plotting, black background or not, regression or not
+  if (is.null(pch)) {
+    pch <- 20
+  }
+  if (bg == TRUE) {
+    if(is.null(col)) {
+      col <- "white"
+    }
+    plot(distTable[, 2], distTable[, 1], type="n", xlab=xlab, ylab=ylab, ...)
+    rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col=bg.col)
+    points(distTable[, 2], distTable[, 1], type="p", col=col, pch=pch)
+    if (regression == TRUE) {
+      abline(fit, col=col, ...)
+    }
+  } else {
+    if(is.null(col)) {
+      col <- "black"
+    }
+    plot(distTable[, 2], distTable[, 1], type="p", xlab=xlab, ylab=ylab, col=col, pch=pch, ...)
+    if (regression == TRUE) {
+      abline(fit, col=col, ...)
+    }
+  }
+  invisible(distTable)
+}
+
